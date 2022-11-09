6e9459f6c253cd7aa7cd1e4f74a66e21ed075db1
afinn02 <affinn@umich.edu>
2022-11-08 20:50:14 -0500

    Need to make some changes to the csv function, wrote test cases

--- f22_Project2.py
+++ f22_Project2.py
@@ -188,7 +188,17 @@ def write_csv(data, filename):
 
     This function should not return anything.
     """
-    pass
+    with open(filename, 'w', newline='') as f:
+   
+        writer = csv.writer(f)
+    
+        #writing first row
+        header = ["Listing Title", "Cost", "Listing ID", "Policy Number", "Place Type", "Number of Bedrooms"]
+        writer.writerow(header)
+        sort = sorted(data, key = lambda x: x[2])
+   
+        for item in sort:
+            writer.writerow(item)
 
 
 def check_policy_numbers(data):
@@ -311,12 +321,13 @@ class TestCases(unittest.TestCase):
         # check that there are 21 lines in the csv
         self.assertEqual(len(csv_lines), 21)
         # check that the header row is correct
-
+        self.assertEqual(csv_lines[0], ["Listing Title", "Cost", "Listing ID", "Policy Number", "Place Type", "Number of Bedrooms"])
         # check that the next row is Private room in Mission District,82,51027324,Pending,Private Room,1
-
+        self.assertEqual(csv_lines[1], ["Private room in Mission District","82","51027324","Pending","Private Room","1"])
         # check that the last row is Apartment in Mission District,399,28668414,Pending,Entire Room,2
+        self.assertEqual(csv_lines[20], ["Apartment in Mission District","399","28668414","Pending","Entire Room","2"])
 
-        pass
+        
 
     def test_check_policy_numbers(self):
         # call get_detailed_listing_database on "html_files/mission_district_search_results.html"
